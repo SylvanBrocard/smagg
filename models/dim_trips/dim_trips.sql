8924043561064350a736c996ec7815d7e18b5339
SylvanBrocard <sylvan.brocard@gmail.com>
2022-01-12 12:21:47 +0100

    added trips dimension

---
+++ models/dim_trips/dim_trips.sql
@@ -0,0 +1,13 @@
+with source_trips as (
+
+    select trip_id, route_id from {{ ref('trips') }}
+
+),
+
+source_places as (
+    select * from {{ ref('places') }}
+)
+
+select source_trips.trip_id, source_trips.route_id, source_places.nb_places
+from source_trips
+inner join source_places on source_trips.route_id = source_places.route_id
